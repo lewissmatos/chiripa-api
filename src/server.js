3b64a236a648c896158668b73623df68140b6a57
Lewis Matos <lewiismb1@example.com>
2021-06-05 09:07:03 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+require('colors')
+
+module.exports = class Server{
+    
+    constructor(){
+        this.app = express()
+        
+        this.port = process.env.PORT
+
+        this.middlewares()
+
+        this.routes()
+    }
+
+    middlewares(){
+        this.app.use(cors())
+        this.app.use(express.json())
+    }
+    
+    listen(){
+        this.app.listen(this.port, ()=> console.log(`Server running at http://localhost:${this.port}`.blue))
+    }
+
+    routes(){
+        this.app.get('/', (req, res) => res.send('API found job App'))
+    }
+
+}
